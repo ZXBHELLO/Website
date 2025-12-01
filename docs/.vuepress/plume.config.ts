7142c46507382d929becbc98a6d07f0fd0e425bd
ZXBHELLO <124427376+ZXBHELLO@users.noreply.github.com>
2025-12-01 13:16:58 +0800

    docs(plume.config.ts): 更新页脚链接和版权信息
    移除了哔哩哔哩链接，并调整了 VuePress 和 Plume 主题的驱动信息展示方式。

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -49,10 +49,9 @@ export default defineThemeConfig({
       '<span style="margin:0 10px">|</span>' +
       '<a href="https://github.com/ZXBHELLO" target="_blank" style="color:var(--vp-c-brand);text-decoration:none;">GitHub</a>' +
       '<span style="margin:0 10px">|</span>' +
-      '<a href="https://space.bilibili.com/1917782316" target="_blank" style="color:var(--vp-c-brand);text-decoration:none;">哔哩哔哩</a>' +
       '</span>',
     copyright: 'Copyright © 2024 ZXBHELLO | ' +
-      '本站由 <a href="https://vuepress.vuejs.org/" target="_blank" style="color:var(--vp-c-brand);text-decoration:none;">VuePress</a> ' +
+      '<a href="https://vuepress.vuejs.org/" target="_blank" style="color:var(--vp-c-brand);text-decoration:none;">VuePress</a> ' +
       '和 <a href="https://theme-plume.vuejs.press/" target="_blank" style="color:var(--vp-c-brand);text-decoration:none;">Plume</a> 强力驱动',
   },
 
